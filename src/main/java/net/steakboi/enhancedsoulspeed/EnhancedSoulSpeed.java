4d70cc9546be66c0d8f487dcc042372f5761bd9b
Timo <feldman.timo@gmail.com>
2024-10-10 19:57:24 -0500

    Refactored tutorial mod to enhanced soul speed

--- src/main/java/net/steakboi/enhancedsoulspeed/EnhancedSoulSpeed.java
+++ src/main/java/net/steakboi/enhancedsoulspeed/EnhancedSoulSpeed.java
@@ -1,4 +1,4 @@
-package net.steakboi.tutorialmod;
+package net.steakboi.enhancedsoulspeed;
 
 import com.mojang.logging.LogUtils;
 import net.minecraftforge.api.distmarker.Dist;
@@ -16,15 +16,15 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(TutorialMod.MOD_ID)
-public class TutorialMod
+@Mod(EnhancedSoulSpeed.MOD_ID)
+public class EnhancedSoulSpeed
 {
     // Define mod id in a common place for everything to reference
-    public static final String MOD_ID = "tutorialmod";
+    public static final String MOD_ID = "enhancedsoulspeed";
     // Directly reference a slf4j logger
     public static final Logger LOGGER = LogUtils.getLogger();
 
-    public TutorialMod()
+    public EnhancedSoulSpeed()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
         modEventBus.addListener(this::commonSetup);
