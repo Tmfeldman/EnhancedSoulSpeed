6a3db2ad6f1c614c0ed2e4adb6ec9fa202bbcd67
Timo <feldman.timo@gmail.com>
2024-10-14 00:18:32 -0500

    updated mappings

--- src/main/java/net/steakboi/enhancedsoulspeed/EnhancedSoulSpeed.java
+++ src/main/java/net/steakboi/enhancedsoulspeed/EnhancedSoulSpeed.java
@@ -1,12 +1,23 @@
 package net.steakboi.enhancedsoulspeed;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
+import net.minecraft.util.Identifier;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class EnhancedSoulSpeed implements ModInitializer {
 	public static final String MOD_ID = "enhancedsoulspeed";
-
+	public static final Identifier SOUL_SPEED_SPRINTING_MODIFIER_ID = Identifier.ofVanilla("soul_speed_sprinting");
+	public static Map<Float, EntityAttributeModifier> SOUL_SPEED_SPEED_MODIFIER_SPRINTING_CACHE = new HashMap<>();
 	@Override
 	public void onInitialize() {
 
+	}
+
+	public static EntityAttributeModifier get_SOUL_SPEED_SPEED_MODIFIER_SPRINTING(Float idx) {
+		if (!SOUL_SPEED_SPEED_MODIFIER_SPRINTING_CACHE.containsKey(idx)) SOUL_SPEED_SPEED_MODIFIER_SPRINTING_CACHE.put(idx, new EntityAttributeModifier(SOUL_SPEED_SPRINTING_MODIFIER_ID, idx, EntityAttributeModifier.Operation.ADD_MULTIPLIED_TOTAL));
+		return SOUL_SPEED_SPEED_MODIFIER_SPRINTING_CACHE.get(idx);
 	}
 }
--- src/main/java/net/steakboi/enhancedsoulspeed/mixin/EnhancedSoulSpeedMixin.java
+++ src/main/java/net/steakboi/enhancedsoulspeed/mixin/EnhancedSoulSpeedMixin.java
@@ -1,15 +1,57 @@
 package net.steakboi.enhancedsoulspeed.mixin;
 
-import net.minecraft.server.MinecraftServer;
+import it.unimi.dsi.fastutil.objects.Object2IntMap;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.Enchantments;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.attribute.EntityAttributeInstance;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
+import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.registry.tag.BlockTags;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(MinecraftServer.class)
-public class EnhancedSoulSpeedMixin {
-	@Inject(at = @At("HEAD"), method = "loadWorld")
-	private void init(CallbackInfo info) {
-		// This code is injected into the start of MinecraftServer.loadWorld()V
+import java.util.Arrays;
+import java.util.Set;
+
+import static net.steakboi.enhancedsoulspeed.EnhancedSoulSpeed.*;
+
+@Mixin(LivingEntity.class)
+public abstract class EnhancedSoulSpeedMixin{
+
+	@Inject(method = "applyMovementEffects(Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/util/math/BlockPos;)V", at = @At(value = "TAIL"))
+	private void AddAndRemoveEnhancedSoulSpeedBoostIfNeeded(ServerWorld pLevel, BlockPos pPos, CallbackInfo ci) {
+		LivingEntity this_cast = (LivingEntity) (Object) this;
+		if (this_cast.getWorld() instanceof ServerWorld && this_cast instanceof PlayerEntity) {
+			try {
+				EntityAttributeInstance attributeinstance = this_cast.getAttributeInstance(EntityAttributes.GENERIC_MOVEMENT_SPEED);
+				attributeinstance.removeModifier(SOUL_SPEED_SPRINTING_MODIFIER_ID);
+				if (this_cast.isSprinting() && this_cast.getWorld().getBlockState(this_cast.getSteppingPos()).isIn(BlockTags.SOUL_SPEED_BLOCKS)) {
+					PlayerEntity Player_this = (PlayerEntity) (Object) this;
+					ItemStack boots = Player_this.getInventory().getArmorStack(0);
+					Set<Object2IntMap.Entry<RegistryEntry<Enchantment>>> Enchantment_set = boots.getEnchantments().getEnchantmentEntries();
+					for (Object2IntMap.Entry<RegistryEntry<Enchantment>> s : Enchantment_set) {
+						if (s.getKey().matchesKey(Enchantments.SOUL_SPEED)) {
+							int soul_speed_level = s.getIntValue();
+							EntityAttributeModifier SOUL_SPEED_SPEED_MODIFIER_SPRINTING = get_SOUL_SPEED_SPEED_MODIFIER_SPRINTING(soul_speed_level*2.0F);
+							attributeinstance.addTemporaryModifier(SOUL_SPEED_SPEED_MODIFIER_SPRINTING);
+							break;
+						}
+					}
+				}
+			} catch (Exception e) {
+				System.out.println("EnhancedSoulSpeed: Failed to decide whether or not to apply enhanced soul speed boost. Continuing");
+				System.out.println(e.toString());
+				System.out.println(Arrays.toString(e.getStackTrace()));
+			}
+		}
 	}
 }
