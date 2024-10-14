3e34e6c7566e1fcb5e6ffe37d8782b443476ce19
Timo <feldman.timo@gmail.com>
2024-10-13 21:22:50 -0500

    refactored from tutorialmod to enhancedsoulspeed

---
+++ src/main/java/net/steakboi/enhancedsoulspeed/EnhancedSoulSpeed.java
@@ -0,0 +1,12 @@
+package net.steakboi.enhancedsoulspeed;
+
+import net.fabricmc.api.ModInitializer;
+
+public class EnhancedSoulSpeed implements ModInitializer {
+	public static final String MOD_ID = "enhancedsoulspeed";
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
