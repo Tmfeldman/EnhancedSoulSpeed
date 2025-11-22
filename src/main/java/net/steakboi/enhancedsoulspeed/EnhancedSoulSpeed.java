92443f84bafd37d8bd271e4cb399030f9aa6a3ec
Timo <feldman.timo@gmail.com>
2025-11-21 18:53:48 -0600

    names updated

--- src/main/java/net/steakboi/enhancedsoulspeed/EnhancedSoulSpeed.java
+++ src/main/java/net/steakboi/enhancedsoulspeed/EnhancedSoulSpeed.java
@@ -1,12 +1,12 @@
-package net.steakboi.timomod;
+package net.steakboi.enhancedsoulspeed;
 
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class TimoMod implements ModInitializer {
-	public static final String MOD_ID = "timomod";
+public class EnhancedSoulSpeed implements ModInitializer {
+	public static final String MOD_ID = "enhancedsoulspeed";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
---
+++ src/main/java/net/steakboi/enhancedsoulspeed/EnhancedSoulSpeedClient.java
@@ -0,0 +1,10 @@
+package net.steakboi.enhancedsoulspeed;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EnhancedSoulSpeedClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
